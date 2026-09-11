b2941c7c759f06ef176de2fb1eb8b9aaa5fba24d
jcagigal-cartoonizer <jcagigal@cartoonizer.com>
2026-09-11 19:33:53 +0200

    2026-09-11

--- src/main/java/com/cartoonizer/rag/shared/utils/ApiKeys.java
+++ src/main/java/com/cartoonizer/rag/shared/utils/ApiKeys.java
@@ -3,5 +3,5 @@ package com.cartoonizer.rag.shared.utils;
 
 public class ApiKeys {
 
-    public static final String OPENAI_API_KEY = "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";
+    public static final String OPENAI_API_KEY = System.getenv("OPENAI_API_KEY");
 }
